13b0d84f74e0fa3730c6e38843b0df72519ee6f8
Dmitriy Shulga <dimashulga1115@gmail.com>
2020-01-23 18:17:24 +0200

    Refactored Code a bit.

--- lib/code.rb
+++ lib/code.rb
@@ -1,22 +1,24 @@
 class Code
-  attr_reader :secret;
+  attr_reader :secret
+
   def initialize(secret)
     @secret = secret;
   end
 
   def compare(guess)
-    return "++++" if secret == guess
+    return '++++' if secret == guess
+    return '' if (secret.each_char.to_a & guess.each_char.to_a).empty?
 
-    result = (0..3).map { |i| guess[i].eql?(secret[i]) ? "+" : ""}
+    places = (0..3).map { |i| guess[i].eql?(secret[i]) ? "+" : "" }
 
-    remain_idx = (0..3).select { |i| result[i] != "+"}
+    remain_idx = (0..3).select { |i| places[i] != "+" }
     remain_guess = remain_idx.map { |i| guess[i] }
     remain_secret = remain_idx.map { |i| secret[i] }
 
     intersection_with_duplicates =
         (remain_secret & remain_guess)
-            .flat_map { |elem| [elem]*[remain_guess.count(elem), remain_secret.count(elem)].min }
+            .flat_map { |elem| [elem] * [remain_guess.count(elem), remain_secret.count(elem)].min }
 
-    result.join << "-"*intersection_with_duplicates.size
+    places.join << "-" * intersection_with_duplicates.size
   end
 end
